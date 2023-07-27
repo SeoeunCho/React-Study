9266d81e04dd961eaff23df5573a9b7e10254e09
seoeuncho <westsilver24@naver.com>
2023-07-28 00:09:52 +0900

    React state, onClick

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [postTitle, changeTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [likes, likeCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <button style={{width: '30px', height: '30px'}} onClick={() => {changeTitle(['여자 코트 추천', '강남 우동맛집', '파이썬독학'])}}></button>
+        <h4>
+          {postTitle[0]}
+          <span onClick={() => {likeCount(likes++)}}>👍</span> {likes}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
