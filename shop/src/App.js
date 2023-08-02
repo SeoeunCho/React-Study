0a637235af8c51a1427c8a67d7e758294dabcf89
seoeuncho <westsilver24@naver.com>
2023-08-02 12:01:53 +0900

    이미지 넣는 법 & public 폴더 이용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import bgImg from './img/bg.png';
 
 function App() {
   return (
@@ -15,6 +16,24 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg" style={{ backgroundImage: 'url(' + bgImg + ')' }}></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
