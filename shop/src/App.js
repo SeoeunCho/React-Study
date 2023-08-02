f6681e20a326c82a592176403721c78f8803d869
seoeuncho <westsilver24@naver.com>
2023-08-02 15:04:35 +0900

    Router 설치 및 세팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes] = useState(data);
@@ -11,32 +12,42 @@ function App() {
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Reactshop</Navbar.Brand>
+          <Navbar.Brand href="/">Reactshop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Detail</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
+      <Routes>
+        <Route path="/" element={<Main shoes={shoes}></Main>} />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+      </Routes>
+    </div>
+  );
+}
 
+function Main(props) {
+  return (
+    <>
+      <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          {shoes.map((el, i) => {
+          {props.shoes.map((el, i) => {
             return <Card shoes={el} idx={i}></Card>;
           })}
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 function Card(props) {
   return (
     <div className="col-md-4">
-      <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} width="80%" />
+      <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} width="80%" alt="" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </div>
