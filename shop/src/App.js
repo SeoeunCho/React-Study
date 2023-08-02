9d449f3caf84205280d09ced4c74573526fccd00
seoeuncho <westsilver24@naver.com>
2023-08-02 12:30:10 +0900

    Card컴포넌트 - data.js import export

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import bgImg from './img/bg.png';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -17,23 +20,25 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg" style={{ backgroundImage: 'url(' + bgImg + ')' }}></div>
+      <div className="main-bg"></div>
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-          </div>
+          {shoes.map((el, i) => {
+            return <Card shoes={el} idx={i}></Card>;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
