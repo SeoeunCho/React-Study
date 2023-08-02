7be96709563be8eb267e4b1d8b40dbcaad3edce2
seoeuncho <westsilver24@naver.com>
2023-08-02 23:18:55 +0900

    커밋테스트

--- shop/src/App.js
+++ shop/src/App.js
@@ -78,7 +78,7 @@ function Card(props) {
     <div className="col-md-4">
       <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} width="80%" alt="" />
       <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.price}</p>
+      <p>{props.shoes.price.toLocaleString()}원</p>
     </div>
   );
 }
