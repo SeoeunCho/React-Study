627fc0c0afc4e2fcf9aad5dc27b14f50560f129f
seoeuncho <westsilver24@naver.com>
2023-08-15 17:59:49 +0900

    성능개선 2 : 재렌더링 막는 useMemo

--- shop/src/pages/Cart.js
+++ shop/src/pages/Cart.js
@@ -1,6 +1,6 @@
 import { Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { memo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 import { changeName, increase } from './../store/useSlice';
 import { addCount } from './../store';
 
@@ -9,10 +9,25 @@ let Child = memo(() => {
   return <div>자식임</div>;
 });
 
+function test() {
+  for (let i = 0; i < 1000000; i++) {
+    return i;
+  }
+}
+
 function Cart() {
   let state = useSelector((state) => state);
   let dispatch = useDispatch();
   let [count, setCount] = useState(0);
+
+  /** useEffect와 useMemo의 차이점
+   * useEffect는 html이 랜더링 된 후 실행
+   * useMemo는 html이 렌더링 될 때 실행
+   * : 실행 시점의 차이
+   */
+  let result = useMemo(() => {
+    return test();
+  }, [count]);
 
   return (
     <div>
