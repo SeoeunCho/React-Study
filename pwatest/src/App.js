19baa2e645da9705926bd4eb03e1f305b026325a
seoeuncho <westsilver24@naver.com>
2023-08-15 19:14:18 +0900

    성능개선 3 : useTransition, useDeferredValue

--- pwatest/src/App.js
+++ pwatest/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import { useState } from 'react';
+import { useDeferredValue, useState, useTransition } from 'react';
+
+let a = new Array(10000).fill(0);
 
 function App() {
   let [name, setName] = useState('');
+  let [isPending, startTransition] = useTransition();
+  let state = useDeferredValue(name);
 
   return (
     <div className="App">
       <input
         onChange={(e) => {
-          setName(e.target.value);
+          startTransition(() => {
+            setName(e.target.value);
+          });
         }}
       />
+      {isPending
+        ? '로딩중'
+        : a.map(() => {
+            return <div>{state}</div>;
+          })}
     </div>
   );
 }
