8c2fc0c2be38296b60627874da5fd2c51baa5188
seoeuncho <westsilver24@naver.com>
2023-08-16 22:53:08 +0900

    Node+Express 서버와 React 연동

--- server/react-project/src/App.js
+++ server/react-project/src/App.js
@@ -1,9 +1,16 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>리액트로 만든 html 페이지입니다.</div>
+
+      <Routes>
+        <Route path="/list" element={<div>리스트페이지</div>}></Route>
+        <Route path="/mypage" element={<div>마이페이지</div>}></Route>
+        {/* <Route path="*" element={<div>없는페이지입니다.</div>}></Route> */}
+      </Routes>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -5,3 +5,23 @@ const app = express();
 app.listen(8080, function () {
   console.log('listening on 8080');
 });
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'react-project/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
+
+// DB데이터 리액트에서 보여줄 때 ex)DB에 있던 상품명을 보여주려면?
+app.get('/product', function (req, res) {
+  res.json({ name: 'black shoes' });
+});
+
+// 리액트라우터 사용하는 경우 경로 설정
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
